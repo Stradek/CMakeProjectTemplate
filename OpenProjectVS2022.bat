3fd369680546d9244f871f1382f1ea3d4894b54b
Piotr Stradowski <stradowski.stradek@gmail.com>
2024-02-20 22:27:17 +0100

    Fix opening hardcoded solution file with batch script
    Now script will search for any .sln file in cmake's build directory and start first one found

--- OpenProjectVS2022.bat
+++ OpenProjectVS2022.bat
@@ -6,4 +6,22 @@ rem Software distributed under the permissive MIT License.
 
 set myPath=%~dp0
 
-start /b %myPath%/build/CMake-VS2022-x64/MultithreadingStudies.sln
+set search_dir="%myPath%/build/CMake-VS2022-x64/"
+
+:: Find the first .sln file
+for /r "%search_dir%" %%i in (*.sln) do (
+    set "sln_file=%%i"
+    goto :open_solution
+)
+
+pause
+
+exit /b
+
+:open_solution
+if defined sln_file (
+    echo Found solution: "%sln_file%"
+    start /b %sln_file%
+) else (
+    echo No .sln file found in %search_dir% and its subdirectories.
+)
