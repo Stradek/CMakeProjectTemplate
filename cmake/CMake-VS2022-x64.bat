a0b35d3db1eb790d51e7d185129feb083cd8a067
Piotr Stradowski <stradowski.stradek@gmail.com>
2024-03-14 20:20:07 +0100

    Generate cmake solution in source
    also add needed ignore rules to gitignore

--- cmake/CMake-VS2022-x64.bat
+++ cmake/CMake-VS2022-x64.bat
@@ -9,12 +9,8 @@ set sourceDir=%1
 set buildDir=%2
 
 if [%sourceDir%] == [] set sourceDir="%myPath%.."
-if [%buildDir%] == [] set buildDir="%sourceDir%\build\CMake-VS2022-x64"
 
 cd %sourceDir%
-
-echo ---- Preparing build directory...
-mkdir %buildDir% >nul 2>&1
 
 where /q cmake.exe
 if %ERRORLEVEL% NEQ 0 (
@@ -23,13 +19,11 @@ if %ERRORLEVEL% NEQ 0 (
 
 echo ---- Generating CMake project for Visual Studio 2022...
 
-cmake.exe -S %sourceDir% -B %buildDir% --preset x64-windows-release
+cmake.exe %sourceDir% --preset x64-windows-release
 
 if %ERRORLEVEL% NEQ 0 (
     goto Error_CMakeBuildingProjectFailed
 )
-
-move %buildDir%\*.sln %sourceDir%
 
 goto:EndCall
 
